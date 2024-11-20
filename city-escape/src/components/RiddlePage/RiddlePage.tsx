565d4c8b211fa7ee9e1617fe949822b1edc0fe4f
Alfarizy <rizy.izy15@gmail.com>
2024-11-20 21:21:28 +0100

    Remove React from "react" import

--- city-escape/src/components/RiddlePage/RiddlePage.tsx
+++ city-escape/src/components/RiddlePage/RiddlePage.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import FeedbackMessage from "../Shared/FeedbackMessage";
 import Button from "../Shared/Button";
