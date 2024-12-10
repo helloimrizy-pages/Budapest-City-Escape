5bdecc829e37d1b18973e84ac585c0551b21d332
Alfarizy <rizy.izy15@gmail.com>
2024-12-10 02:59:46 +0100

    Add route

--- city-escape/src/App.tsx
+++ city-escape/src/App.tsx
@@ -1,15 +1,31 @@
 import "leaflet/dist/leaflet.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Map from "./components/Map/Map.tsx";
-import RiddlePage from "./components/RiddlePage/RiddlePage.tsx";
+import Navbar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Map from "./components/Map/Map";
+import RiddlePage from "./components/RiddlePage/RiddlePage";
+import HowItWorks from "./pages/HowItWorks/HowItWorks";
+import FAQ from "./pages/FAQ/FAQ";
+import AboutUs from "./pages/AboutUs/AboutUs";
+import ContactUs from "./pages/ContactUs/ContactUs";
 
-export default function App() {
+const App = () => {
   return (
     <Router>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Map />} />
-        <Route path="/riddle" element={<RiddlePage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/riddle/:riddleId" element={<RiddlePage />} />
+        <Route path="/how-it-works" element={<HowItWorks />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<ContactUs />} />
       </Routes>
+      <Footer />
     </Router>
   );
-}
+};
+
+export default App;
